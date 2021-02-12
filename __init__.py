3d0d2f47d29d92a58b235fabc5e465174026a18d
EoralMilk <1322028031@qq.com>
2021-02-12 12:12:49 +0800

    <py+blend>Add a single frame rendering function. Add an info of render complete.

--- __init__.py
+++ __init__.py
@@ -23,6 +23,8 @@ def Settings():
         name="Center", default="Center")
     bpy.types.Scene.ORA_Faces = bpy.props.IntProperty(
         name="Faces", default=8, min=1, max=256)
+    bpy.types.Scene.ORA_OneFrame = bpy.props.BoolProperty(
+        name="One Frame", default=False, description="Whether to render only the current frame")
 
 
 class ORA_PT_Render(PanelSetting, bpy.types.Panel):
@@ -38,13 +40,15 @@ class ORA_PT_Render(PanelSetting, bpy.types.Panel):
         scene = context.scene
 
         row = layout.row()
-        row.label(text="Center object")
+        row.label(text="Center object:")
         row = layout.row()
         row.prop(scene, 'ORA_Center', text="Name", icon="EMPTY_DATA")
         row = layout.row()
-        row.label(text="Render Faces")
+        row.label(text="Render Settings:")
         row = layout.row()
         row.prop(scene, 'ORA_Faces', text="Faces", icon="SPHERE")
+        row = layout.row()
+        row.prop(scene, 'ORA_OneFrame', text="One Frame")
         row = layout.row()
         row.operator("ora.render", text="Start Render",
                      icon="OUTLINER_DATA_CAMERA")
@@ -59,6 +63,7 @@ class ORA_OT_Render(bpy.types.Operator):
         pi2 = 6.28319
         center = context.scene.objects[context.scene.ORA_Center]
         faces = context.scene.ORA_Faces
+        anim = not context.scene.ORA_OneFrame
 
         RGBOs = context.scene.node_tree.nodes["RGB Output"]
         ShadowOs = context.scene.node_tree.nodes["Shadow Output"]
@@ -73,13 +78,14 @@ class ORA_OT_Render(bpy.types.Operator):
             ShadowOs.file_slots[0].path = ShadowOs_DefName + strfacing
 
             # rendering
-            bpy.ops.render.render(animation=True, use_viewport=True)
+            bpy.ops.render.render(animation=anim, use_viewport=True)
 
             center.rotation_euler[2] += pi2/faces
 
         center.rotation_euler[2] = defRot
         RGBOs.file_slots[0].path = RGBOs_DefName
         ShadowOs.file_slots[0].path = ShadowOs_DefName
+        self.report({"INFO"}, "Render Complete!")
 
         return {"FINISHED"}
 
