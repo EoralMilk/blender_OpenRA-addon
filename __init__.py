b91681595e0bd4f462d75d3bd09781f91e8a3843
EoralMilk <1322028031@qq.com>
2021-02-12 21:05:28 +0800

    <py+blend> add output settings

--- __init__.py
+++ __init__.py
@@ -10,6 +10,8 @@ bl_info = {
     "wiki_url": "",
     "category": "OpenRA"}
 
+# 面板基础设定
+
 
 class PanelSetting:
     bl_space_type = "VIEW_3D"
@@ -17,15 +19,25 @@ class PanelSetting:
     bl_category = "ORA"
     bl_context = "objectmode"
 
+# 全部RNA属性
+
 
 def Settings():
     bpy.types.Scene.ORA_Center = bpy.props.StringProperty(
         name="Center", default="Center")
     bpy.types.Scene.ORA_Faces = bpy.props.IntProperty(
         name="Faces", default=8, min=1, max=256)
+
     bpy.types.Scene.ORA_OneFrame = bpy.props.BoolProperty(
         name="One Frame", default=False, description="Whether to render only the current frame")
+    bpy.types.Scene.ORA_UseAlpha = bpy.props.BoolProperty(
+        name="Use Alpha", default=False, description="Whether to render only with Alpha")
 
+    bpy.types.Scene.ORA_OutPath = bpy.props.StringProperty(
+        name="Output Path", description="Change the image output path", default="D: \\Blender-Desk\\OUT PUT\\")
+
+
+# 渲染总面板
 
 class ORA_PT_Render(PanelSetting, bpy.types.Panel):
     bl_idname = "ora.render"
@@ -43,33 +55,70 @@ class ORA_PT_Render(PanelSetting, bpy.types.Panel):
         row.label(text="Center object:")
         row = layout.row()
         row.prop(scene, 'ORA_Center', text="Name", icon="EMPTY_DATA")
-        row = layout.row()
-        row.label(text="Render Settings:")
-        row = layout.row()
-        row.prop(scene, 'ORA_Faces', text="Faces", icon="SPHERE")
-        row = layout.row()
-        row.prop(scene, 'ORA_OneFrame', text="One Frame")
+
         row = layout.row()
         row.operator("ora.render", text="Start Render",
                      icon="OUTLINER_DATA_CAMERA")
 
+# 渲染设置面板
 
+
+class ORA_PT_RenderSet(PanelSetting, bpy.types.Panel):
+    bl_idname = "ora.renderset"
+    bl_label = "Render Settings"
+    bl_parent_id = "ora.render"
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        row = layout.row()
+        row.prop(scene, 'ORA_Faces', text="Faces", icon="SPHERE")
+        row = layout.row()
+        row.prop(scene, 'ORA_OneFrame', text="One Frame")
+
+# 输出设置面板
+
+
+class ORA_PT_OutputSet(PanelSetting, bpy.types.Panel):
+    bl_idname = "ora.outputset"
+    bl_label = "Output Settings"
+    bl_parent_id = "ora.render"
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        row = layout.row()
+        row.prop(scene, 'ORA_UseAlpha', text="Use Alpha")
+        row = layout.row()
+        row.prop(scene, 'ORA_OutPath', text="Path")
+
+        row = layout.row()
+        row.operator("ora.setup", text="Use Output settings",
+                     icon="CHECKMARK")
+
+
+# 渲染器
 class ORA_OT_Render(bpy.types.Operator):
     bl_idname = "ora.render"
     bl_label = "Render"
     bl_options = {"REGISTER"}
 
     def execute(self, context):
+        # Common
         pi2 = 6.28319
+        # Base Settings
         center = context.scene.objects[context.scene.ORA_Center]
         faces = context.scene.ORA_Faces
+        # BoolSwitch
         anim = not context.scene.ORA_OneFrame
-
+        # Out Put Path
         RGBOs = context.scene.node_tree.nodes["RGB Output"]
         ShadowOs = context.scene.node_tree.nodes["Shadow Output"]
         RGBOs_DefName = RGBOs.file_slots[0].path
         ShadowOs_DefName = ShadowOs.file_slots[0].path
-
+        # Rotation
         defRot = center.rotation_euler[2]
 
         for facing in range(faces):
@@ -90,9 +139,39 @@ class ORA_OT_Render(bpy.types.Operator):
         return {"FINISHED"}
 
 
+# 启用输出设置
+class ORA_OT_SetUp(bpy.types.Operator):
+    bl_idname = "ora.setup"
+    bl_label = "SetUp"
+    bl_options = {"REGISTER"}
+
+    def execute(self, context):
+
+        # BoolSwitch
+        alpha = context.scene.ORA_UseAlpha
+
+        # Out Put Path
+        output = context.scene.ORA_OutPath
+
+        # SetUp
+        context.scene.node_tree.nodes["AlphaSwitch"].check = alpha
+        context.scene.node_tree.nodes["AlphaSwitch-Shadow"].check = alpha
+
+        context.scene.node_tree.nodes["RGB Output"].base_path = output
+        context.scene.node_tree.nodes["Shadow Output"].base_path = output
+
+        self.report({"INFO"}, "Settings enabled")
+
+        return {"FINISHED"}
+
+
+# 全部类注册表
 classes = [
     ORA_PT_Render,
-    ORA_OT_Render
+    ORA_PT_RenderSet,
+    ORA_PT_OutputSet,
+    ORA_OT_Render,
+    ORA_OT_SetUp
 ]
 
 
